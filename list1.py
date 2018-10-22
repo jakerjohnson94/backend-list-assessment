7cb4834e81296ccaba24c4e3c59d79576b731f2a
Jake Johnson <38015741+jakerjohnson94@users.noreply.github.com>
2018-10-22 15:40:26 -0400

    Completed list1.py

--- list1.py
+++ list1.py
@@ -22,9 +22,10 @@
 # strings where the string length is 2 or more and the first
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
+
+
 def match_ends(words):
-    """Your code goes here.  Edit this docstring."""
-    return
+    return len(filter(lambda x: len(x) > 1 and x[0] == x[-1], words))
 
 
 # B. front_x
@@ -35,8 +36,7 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-    """Your code goes here.  Edit this docstring."""
-    return
+    return sorted(filter(lambda word: word.startswith('x'), words)) + sorted(filter(lambda word: not word.startswith('x'), words))
 
 
 # C. sort_last
@@ -46,8 +46,7 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-    """Your code goes here.  Edit this docstring."""
-    return
+    return sorted(tuples, key=lambda x: x[1])
 
 
 # Simple provided test() function used in main() to print
